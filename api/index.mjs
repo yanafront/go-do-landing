38ccf149fdb9032567a6b4db1c08e83c05eff4ad
yanadubeshko <ya.dubeshko@seowork.ru>
2025-09-28 16:20:40 +0300

    update config app

--- api/index.mjs
+++ api/index.mjs
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
@@ -73,4 +73,4 @@ app.get('*', (req, res) => {
   `);
 });
 
-module.exports = app;
+export default app;
